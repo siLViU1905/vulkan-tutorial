911d2c9d79a2b66cc8de9d0e68169286d1a91a1b
SiLViU <silviuptc@gmail.com>
2025-08-09 20:39:39 +0300

    fixed mistake

---
+++ main.cpp
@@ -0,0 +1,22 @@
+#include <array>
+#include <iostream>
+
+#include "include/GLFW/glfw3.h"
+#include <vulkan/vulkan.h>
+
+#include "app/VulkanApp.h"
+
+int main()
+{
+  std::unique_ptr<VulkanApp> app;
+
+  try
+  {
+    app = std::make_unique<VulkanApp>();
+  }
+  catch (const std::exception& e)
+  {
+    std::cout << e.what() << '\n';
+    return -1;
+  }
+}
