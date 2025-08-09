a9361cd4724c688ba163ef4458423db9cfdb8f3e
SiLViU <silviuptc@gmail.com>
2025-08-09 20:32:59 +0300

    cmake setup

---
+++ app/VulkanApp.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Silviu on 09.08.2025.
+//
+
+#include "VulkanApp.h"
---
+++ app/VulkanApp.h
@@ -0,0 +1,16 @@
+//
+// Created by Silviu on 09.08.2025.
+//
+
+#ifndef VULKANAPP_H
+#define VULKANAPP_H
+
+
+
+class VulkanApp {
+
+};
+
+
+
+#endif //VULKANAPP_H
